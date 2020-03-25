cdc0f115c5fa22bfdc08c72ec1ae09a67d2367fb
Santu Koley <santu.koley@ingreens.in>
2020-03-25 17:59:56 +0530

    Initial commit

--- src/SimplePanel.tsx
+++ src/SimplePanel.tsx
@@ -1,8 +1,34 @@
 import React, { PureComponent } from 'react';
 import { PanelProps } from '@grafana/data';
 import { SimpleOptions } from 'types';
+import { Doughnut } from 'react-chartjs-2';
 
-interface Props extends PanelProps<SimpleOptions> {}
+interface Props extends PanelProps<SimpleOptions> { }
+
+const state = {
+  labels: ['January', 'February', 'March',
+    'April', 'May'],
+  datasets: [
+    {
+      label: 'Rainfall',
+      backgroundColor: [
+        '#B21F00',
+        '#C9DE00',
+        '#2FDE00',
+        '#00A6B4',
+        '#6800B4'
+      ],
+      hoverBackgroundColor: [
+        '#501800',
+        '#4B5000',
+        '#175000',
+        '#003350',
+        '#35014F'
+      ],
+      data: [65, 59, 80, 81, 56]
+    }
+  ]
+}
 
 export class SimplePanel extends PureComponent<Props> {
   render() {
@@ -16,22 +42,20 @@ export class SimplePanel extends PureComponent<Props> {
           height,
         }}
       >
-        <svg
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
+        <Doughnut
+          data={state}
+          options={{
+            title: {
+              display: true,
+              text: 'Average Rainfall per month',
+              fontSize: 20
+            },
+            legend: {
+              display: true,
+              position: 'right'
+            }
           }}
-          width={width}
-          height={height}
-          xmlns="http://www.w3.org/2000/svg"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-          viewBox={`-${width / 2} -${height / 2} ${width} ${height}`}
-        >
-          <g>
-            <circle style={{ fill: '#32a852' }} r={100} />
-          </g>
-        </svg>
+        />
 
         <div
           style={{
